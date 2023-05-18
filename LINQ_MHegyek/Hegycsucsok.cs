c7544b9674456e8650d059afcb71bfdbca68bc72
ddaniel <danko7119@gmail.com>
2023-05-18 14:20:30 +0200

    Add project files.

---
+++ LINQ_MHegyek/Hegycsucsok.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_MHegyek
+{
+    internal class Hegycsucsok
+    {
+        string nev;
+        string hegyseg;
+        int magassag;
+
+        public Hegycsucsok(string nev, string hegyseg, int magassag)
+        {
+            this.nev = nev;
+            this.hegyseg = hegyseg;
+            this.magassag = magassag;
+        }
+        public string Nev { get => nev; }
+        public string Hegyseg { get => hegyseg; }
+        public int Magassag { get => magassag; }
+    }
+}
---
+++ LINQ_MHegyek/Program.cs
@@ -0,0 +1,32 @@
+﻿using LINQ_MHegyek;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<Hegycsucsok> HegycsucsLista = new List<Hegycsucsok>();
+        StreamReader sr = new StreamReader("hegyekMo.txt");
+        sr.ReadLine();
+
+        while (!sr.EndOfStream)
+        {
+            string[] hegycsucs = sr.ReadLine().Split(";");
+            HegycsucsLista.Add(new Hegycsucsok(hegycsucs[0], hegycsucs[1], int.Parse(hegycsucs[2])));
+        }
+        sr.Close();
+
+
+        Console.WriteLine($"3.feladat: Hegycsúcsok száma: {HegycsucsLista.Count()} db");
+        Console.WriteLine($"4.feladat: Hegycsúcsok átlagos magassága: {Math.Round(HegycsucsLista.Average(x => x.Magassag), 2)} m");
+
+        Hegycsucsok legmagasabbHegy = HegycsucsLista.OrderByDescending(h => h.Magassag).First();
+
+        Console.WriteLine("5. feladat: A legmagasabb hegycsúcs adatai:");
+        Console.WriteLine($"\tNév: {legmagasabbHegy.Nev}");
+        Console.WriteLine($"\tHegység: {legmagasabbHegy.Hegyseg}");
+        Console.WriteLine($"\tMagasság: {legmagasabbHegy.Magassag} m");
+
+        int bekertMagassagertek = int.Parse(Console.ReadLine());
+
+    }
+}
